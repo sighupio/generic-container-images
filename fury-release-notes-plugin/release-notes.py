600b72cbbb4ff070c923fc349cd09d5f3bd47563
Ramiro Algozino <ramiro@sighup.io>
2025-06-09 10:41:53 +0200

    feat(fury-release-notes-plugin): support revisions Add support for versions like vX.Y.Z-rev.W

--- fury-release-notes-plugin/release-notes.py
+++ fury-release-notes-plugin/release-notes.py
@@ -1,18 +1,26 @@
 import os
 from shutil import copyfile
 
-import semantic_version # pylint: disable=import-error
+import semantic_version  # pylint: disable=import-error
 
 RELEASE_NOTES_FILE_PATH = os.getenv("RELEASE_NOTES_FILE_PATH")
 DRONE_TAG = os.getenv("DRONE_TAG")
 
 if __name__ == "__main__":
-    version = DRONE_TAG[1:]
+    version = DRONE_TAG.lstrip('v')  # Remove the v prefix if present
 
-    major = semantic_version.Version(version).major
-    minor = semantic_version.Version(version).minor
-    patch = semantic_version.Version(version).patch
+    sversion = semantic_version.Version(version)
 
+    major = sversion.major
+    minor = sversion.minor
+    patch = sversion.patch
+
+    # This allows us to use the "final" release notes also for Release Candidates
     release_notes_file = f"./docs/releases/v{major}.{minor}.{patch}.md"
+
+    # We use <version>-rev.1 for revisions:
+    if sversion.prerelease[0] == "rev":
+        release_notes_file = f"./docs/releases/v{version}.md"
+
     print(f"Copying from {release_notes_file} to {RELEASE_NOTES_FILE_PATH}")
     copyfile(release_notes_file, RELEASE_NOTES_FILE_PATH)
